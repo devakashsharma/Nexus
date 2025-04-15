c56dc99c33d7590f6c4b719d269d6ecc7249722a
Akash Sharma <soulakash20002@gmail.com>
2025-04-15 10:01:37 +0530

    initial setup

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask # type: ignore
+from flask import Flask, render_template # type: ignore
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 app.run(debug=True)
